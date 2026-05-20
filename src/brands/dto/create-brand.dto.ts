9d679554a1dfcb73e6ee7460b22b43a23aa0ecb3
Eduardo Fleitas <edufleitas227@gmail.com>
2026-05-20 15:47:55 -0300

    A seed resource was created and brand resources were updated.

--- src/brands/dto/create-brand.dto.ts
+++ src/brands/dto/create-brand.dto.ts
@@ -1,1 +1,7 @@
-export class CreateBrandDto {}
+import { IsString, isString, MinLength } from "class-validator";
+
+export class CreateBrandDto {
+    @IsString()
+    @MinLength(1)
+    name!:string;
+}
